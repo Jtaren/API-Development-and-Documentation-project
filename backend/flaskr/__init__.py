71bd2be9a15ce4826f7551f7bc45dcc71be0f5a1
Solomon Jt Aren <78317210+Jtaren@users.noreply.github.com>
2024-03-10 21:45:58 +0100

    Update __init__.py

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -13,6 +13,14 @@ def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
 
+    @app.route('/')
+    def hello():
+        return jsonify(('message': 'HELLO WORLD'))
+
+    @app.route('/smiley')
+    def smiley():
+        return ':)'
+
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
